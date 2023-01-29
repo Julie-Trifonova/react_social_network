8606c0a64edd1a485bb929fa419d7dcd52456c0b
Juliya <yula_sakura@list.ru>
2023-01-29 14:24:15 +0300

    commit

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,35 +1,57 @@
-import React from 'react'
 import PropTypes from 'prop-types'
-import styles from './users.module.css'
+import React, { Component } from 'react'
 import {nanoid} from "nanoid";
+import userPhoto from "../../assets/images/user_1.jpg";
+import styles from "./users.module.css";
 import axios from "axios";
-import userPhoto from '../../assets/images/user_1.jpg'
 
-const Users = (props) => {
+export class Users extends Component {
 
-    if (props.users.length === 0) {
-
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
             });
     }
 
-    return (
-        <div>
-            {
-                props.users.map(u => <div key={nanoid()}>
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
+    }
+
+    render() {
+
+        let pagesCount = Math.ceil (this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+        for(let i = 1; i <= pagesCount; i++ ) {
+            pages.push(i);
+        }
+
+        return <div>
+            <div>
+                {pages.map(p => {
+                    return <span className={this.props.currentPage === p ? styles.selectedPage : 'undefined' }
+                    onClick={(e) => {this.onPageChanged(p)}}>{p}</span>
+                })}
+            </div>
+                {
+                    this.props.users.map(u => <div key={nanoid()}>
                     <span>
                         <div>
                             <img src={u.photos.small !== null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                         </div>
                         <div>
                             { u.followed
-                                ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                                : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+                                ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
+                                : <button onClick={() => {this.props.follow(u.id)}}>Follow</button>}
                         </div>
                     </span>
-                    <span>
+                        <span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -39,12 +61,11 @@ const Users = (props) => {
                             {/*<div>{u.location.city}</div>*/}
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
-    )
-}
+                    </div>)
+                }
+            </div>
 
-Users.propTypes = {}
+    }
+}
 
 export default Users
