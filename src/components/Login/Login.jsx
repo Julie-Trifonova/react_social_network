dcb578a8159b208d076482ab3c519597108dd441
Juliya <yula_sakura@list.ru>
2023-04-24 10:34:23 +0300

    78_дщ9_stop-submit, redux-form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import {required} from "../../utils/validators/validators";
 import {connect} from "react-redux";
 import {login} from "../../redux/authReducer";
 import {Navigate} from "react-router-dom";
+import style from './../common/FormsControls/FormsControls.module.css'
 
 const LoginForm = (props) => {
     return (
@@ -31,6 +32,11 @@ const LoginForm = (props) => {
                     type={'checkbox'}
                 /> remember me
             </div>
+            { props.error &&
+                <div className={style.formSummaryError}>
+                    {props.error}
+                </div>
+            }
             <div>
                 <button>Login</button>
             </div>
