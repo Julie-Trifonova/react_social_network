2cfdb0fae9261abea180c8ca040f7881c97e2d5e
Juliya <yula_sakura@list.ru>
2023-04-27 15:45:44 +0300

    89_tests, jest

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -4,6 +4,7 @@ import {profileAPI, usersAPI} from "../components/api/api";
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
+const DELETE_POST = 'DELETE_POST';
 
 let initialState = {
     posts: [
@@ -41,6 +42,12 @@ const profileReducer = ( state = initialState, action ) => {
                 profile: action.profile
             };
         }
+        case DELETE_POST: {
+            return {
+                ...state,
+                posts: action.posts.filter(p => p.id !== action.postId)
+            };
+        }
         default:
             return state;
         }
@@ -55,6 +62,9 @@ export const setUserProfile = (profile) => ({
 })
 export const setStatus = (status) => ({
     type: SET_STATUS, status
+})
+export const deletePost = (postId) => ({
+    type: DELETE_POST, postId
 })
 
 export const getUserProfile = (userId) => (dispatch) => {
---
+++ src/redux/profileReducer.test.js
@@ -0,0 +1,46 @@
+import profileReducer, {addPostActionCreator, deletePost} from "./profileReducer";
+
+let state = {
+    posts: [
+        { id: 1, message: "Hi, how are you?", likesCount: 5 },
+        { id: 2, message: "It`s my first post", likesCount: 27 },
+    ]
+};
+
+test('length of posts should be incremented', () => {
+    // 1. test data
+    let action = addPostActionCreator('Hello guys')
+    // 2. action
+    let newState = profileReducer(state, action)
+    // 3. expectation
+    expect(newState.posts.length).toBe(3);
+    expect(newState.posts[3].message).toBe('Hello guys')
+});
+
+test('message of new post should be correct', () => {
+    // 1. test data
+    let action = addPostActionCreator('Hello guys')
+    // 2. action
+    let newState = profileReducer(state, action)
+    // 3. expectation
+    expect(newState.posts[3].message).toBe('Hello guys')
+});
+
+test('after deleting, length of messages should be decrement', () => {
+    // 1. test data
+    let action = deletePost(1)
+    // 2. action
+    let newState = profileReducer(state, action)
+    // 3. expectation
+    expect(newState.posts.length).toBe(1)
+});
+
+test('after deleting, length shouldn`t be decrement if id is incorrect', () => {
+    // 1. test data
+    let action = deletePost(109)
+    // 2. action
+    let newState = profileReducer(state, action)
+    // 3. expectation
+    expect(newState.posts.length).toBe(2)
+});
+
