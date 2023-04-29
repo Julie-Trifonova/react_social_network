9c24f06d35b3d32d2bf78fadfcbfd40ffe2d01ab
Juliya <yula_sakura@list.ru>
2023-04-29 08:03:13 +0300

    94_lazy, susoense

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {lazy} from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+// const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'))
+// const ProfileContainer = lazy(() => import('./components/Profile/ProfileContainer'))
+
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
@@ -13,6 +17,7 @@ import {compose} from "redux";
 import {initializeApp} from "./redux/appReducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from "./redux/reduxStore";
+import {withSuspense} from "./hoc/withSuspense";
 
 class App extends React.Component {
     componentDidMount() {
@@ -20,7 +25,7 @@ class App extends React.Component {
     }
 
     render() {
-        if(!this.props.initialized) {
+        if (!this.props.initialized) {
             return <Preloader/>
         }
         return (
@@ -29,8 +34,21 @@ class App extends React.Component {
                 <Navbar/>
                 <div className="app-wrapper-content">
                     <Routes>
-                        <Route exact path="/dialogs" element={<DialogsContainer/>}/>
-                        <Route exact path="/profile/:userId?" element={<ProfileContainer/>}/>
+                        <Route
+                            exact
+                            path="/dialogs"
+                            element={
+                            <DialogsContainer/>
+                            // withSuspense(DialogsContainer)
+                        }
+                        />
+                        <Route
+                            exact
+                            path="/profile/:userId?"
+                            element={<ProfileContainer/>
+                            // withSuspense(ProfileContainer)
+                        }
+                        />
                         <Route exact path="/users" element={<UsersContainer/>}/>
                         <Route exact path="/login" element={<Login/>}/>
                     </Routes>
